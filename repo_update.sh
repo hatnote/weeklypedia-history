26a069af2a88ec1f9141422e6cc3b1a216c0ad58
Mahmoud Hashemi <mahmoud@hatnote.com>
2015-08-02 10:18:07 -0700

    adding initial backup/upload script

---
+++ repo_update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function log_and_die { logger $1; exit 1; }
+
+DATE_STR="$(date '+%l:%M%P  %B %e, %Y')"
+
+cd /home/hatnote/weeklypedia/static
+if [ "$(git status -s)" ]
+  then
+    git add .
+    git commit -a -m "autocommit $DATE_STR" || log_and_die "WPH: Failed to commit ($DATE_STR)"
+    COMMIT_ID="$(git rev-parse HEAD)"
+    git push origin master || log_and_die "WPH: Failed to push ($DATE_STR)"
+    logger "WPH: Successfully committed/pushed '$COMMIT_ID' ($DATE_STR)."
+  else
+    logger "WPH: No tracked/trackable files updated. ($DATE_STR)"
+fi
